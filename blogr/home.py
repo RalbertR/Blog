0ac9a96c5a3238e99b9be73fb0830fbbd413173a
RalbertR <raulrobino@gmail.com>
2025-07-10 11:19:54 -0300

    Desarrollo inicial de frontend

--- blogr/home.py
+++ blogr/home.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('home', __name__)
 
 @bp.route('/')
 def index():
-    return 'Pagina de inicio'
+    return render_template('index.html')
 
 @bp.route('/')
 def blog():
-    return 'Pagina de blog'
+    return render_template('blog.html')
