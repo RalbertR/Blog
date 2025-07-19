d4d28722c4db436a7ca8a44589bfea5cebac35aa
RalbertR <raulrobino@gmail.com>
2025-07-19 07:45:33 -0300

    Implementado seccion Profile, con capacidad de mofiicar usuario logueado

--- blogr/post.py
+++ blogr/post.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for, redirect
 
 bp = Blueprint('post', __name__, url_prefix='/post')
 
 @bp.route('/posts')
 def posts():
-    return 'Pagina de publicaciones'
+    return redirect(url_for('home.index'))
 
 @bp.route('/create')
 def create():
